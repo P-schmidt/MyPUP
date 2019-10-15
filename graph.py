764e03dcbee4dcb6db55b63043f57a9f86f4108f
Unknown <tidi.stam@gmail.com>
2019-10-15 10:43:04 +0200

    Eerste versie class Graph en Node toegevoegd

---
+++ graph.py
@@ -0,0 +1,72 @@
+
+class Graph():
+
+    def __init__(self, data):
+        # initializes the nodes and edges
+        self.start = "Mypup"
+        self.goal = "Mypup"
+        self.nodes = {}
+
+        # Dictionary where key is address and value is node
+        # object
+        self.node_address = {}
+
+        # Create all the nodes in the grid
+        self.make_node()
+        print("node done")
+
+        # Create all the edges
+        self.create_edges()
+    
+    def make_node(self):
+        # loop through list of nodes and add them
+        if self.goal != None:
+            self.add_node(self.goal)
+        else:
+            for address in data.keys():
+                self.add_node(address)
+
+    def add_node(self, address):
+        """
+        Adds a node to self.nodes with (source) address and
+        loadtime
+        """
+        tmp = Node(address)
+        self.node_address[address] = 
+
+
+class Node():
+    """
+    Node object. This a location in our grid, also includes the
+    loadtime attribute which specifies the approximate loading time
+    of the address.
+    """
+    
+    def __init__(self, address, loadtime, attr):
+        self.address = address
+        self.loadtime = loadtime
+        self.attr = attr
+        self.edges = []
+
+    @property
+    def loadtime(self):
+        return self.loadtime
+
+    @property
+    def attr(self):
+        return self.attr
+
+    def add_edge(self, edge):
+        self.edges.append(edge)
+
+    def delete_edge(self, edge):
+        for e in self.edges:
+            if e == edge:
+                self.edges.remove(e)
+
+    def get_edges(self):
+        for edge in self.edges:
+            yield edge
+
+    def get_address(self):
+        return self.address
