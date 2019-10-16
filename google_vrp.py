4e099db28336f9d1508d37a5f007697b3a868922
Unknown <tidi.stam@gmail.com>
2019-10-16 14:40:09 +0200

    Csv bestanden hebben nu loadtimes, vrp is nu including demands

--- google_vrp.py
+++ google_vrp.py
@@ -26,7 +26,11 @@ def create_database(filename, create=True):
     else:
         with open('duration_db.pkl', 'rb') as f:
             data = pickle.load(f)
-            
+    
+    data['demands'] = random.sample(range(12,120), len(data['distance_matrix']))
+    print(sum(data['demands']))
+    data['demands'][0] = 0
+    data['vehicle_capacities'] = [1200, 1200, 1200, 1200, 1200]
     return data
 
  
@@ -114,24 +118,59 @@ def create_data_model():
     data['depot'] = 0
     return data
 
-def print_solution(data, manager, routing, solution):
-    """Prints solution on console."""
-    max_route_distance = 0
+# def print_solution(data, manager, routing, assignment):
+#     """Prints assignment on console."""
+#     total_distance = 0
+#     total_load = 0
+#     for vehicle_id in range(data['num_vehicles']):
+#         index = routing.Start(vehicle_id)
+#         plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+#         route_distance = 0
+#         route_load = 0
+#         while not routing.IsEnd(index):
+#             node_index = manager.IndexToNode(index)
+#             route_load += data['loadtimes'][node_index]
+#             plan_output += ' {0} Load({1}) -> '.format(node_index, route_load)
+#             previous_index = index
+#             index = assignment.Value(routing.NextVar(index))
+#             route_distance += routing.GetArcCostForVehicle(
+#                 previous_index, index, vehicle_id)
+#         plan_output += ' {0} Load({1})\n'.format(manager.IndexToNode(index),
+#                                                  route_load)
+#         plan_output += 'Travelling time of the route: {}minutes\n'.format(round(route_distance/60))
+#         plan_output += 'Loading time of the route: {} minutes\n'.format(round(route_load/60))
+#         print(plan_output)
+#         total_distance += route_distance
+#         total_load += route_load
+#     print('Total travelling time of all routes: {}minutes'.format(round(total_distance/60)))
+#     print('Total loadingtime of all routes: {}minutes'.format(round(total_load/60)))
+
+def print_solution(data, manager, routing, assignment):
+    """Prints assignment on console."""
+    total_distance = 0
+    total_load = 0
     for vehicle_id in range(data['num_vehicles']):
         index = routing.Start(vehicle_id)
         plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
         route_distance = 0
+        route_load = 0
         while not routing.IsEnd(index):
-            plan_output += ' {} -> '.format(manager.IndexToNode(index))
+            node_index = manager.IndexToNode(index)
+            route_load += data['demands'][node_index]
+            plan_output += ' {0} Load({1}) -> '.format(node_index, route_load)
             previous_index = index
-            index = solution.Value(routing.NextVar(index))
+            index = assignment.Value(routing.NextVar(index))
             route_distance += routing.GetArcCostForVehicle(
                 previous_index, index, vehicle_id)
-        plan_output += '{}\n'.format(manager.IndexToNode(index))
+        plan_output += ' {0} Load({1})\n'.format(manager.IndexToNode(index),
+                                                 route_load)
         plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+        plan_output += 'Load of the route: {}\n'.format(route_load)
         print(plan_output)
-        max_route_distance = max(route_distance, max_route_distance)
-    print('Maximum of the route distances: {}m'.format(max_route_distance))
+        total_distance += route_distance
+        total_load += route_load
+    print('Total distance of all routes: {}m'.format(total_distance))
+    print('Total load of all routes: {}'.format(total_load))
 
 def main():
     """Solve the CVRP problem."""
@@ -159,16 +198,22 @@ def main():
     # Define cost of each arc.
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-    # Add Distance constraint.
-    dimension_name = 'Distance'
-    routing.AddDimension(
-        transit_callback_index,
-        0,  # no slack
-        18000,  # vehicle maximum travel distance
+
+    # Add Capacity constraint.
+    def demand_callback(from_index):
+        """Returns the demand of the node."""
+        # Convert from routing variable Index to demands NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        return data['demands'][from_node]
+
+    demand_callback_index = routing.RegisterUnaryTransitCallback(
+        demand_callback)
+    routing.AddDimensionWithVehicleCapacity(
+        demand_callback_index,
+        0,  # null capacity slack
+        data['vehicle_capacities'],  # vehicle maximum capacities
         True,  # start cumul to zero
-        dimension_name)
-    distance_dimension = routing.GetDimensionOrDie(dimension_name)
-    distance_dimension.SetGlobalSpanCostCoefficient(100)
+        'Capacity')
 
     # Setting first solution heuristic.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
@@ -176,11 +221,11 @@ def main():
         routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
 
     # Solve the problem.
-    solution = routing.SolveWithParameters(search_parameters)
+    assignment = routing.SolveWithParameters(search_parameters)
 
     # Print solution on console.
-    if solution:
-        print_solution(data, manager, routing, solution)
+    if assignment:
+        print_solution(data, manager, routing, assignment)
 
 
 if __name__ == '__main__':
